01c53c7a275033b5c140ca3a91ae3ac611cfa9bc
PonikTrofik <izanami369@gmail.com>
2020-02-23 23:55:12 +0100

    1

---
+++ WebApplication3/Controllers/FormHandlerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class FormHandlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection)
+        {
+            ViewBag.Firstname = formCollection["FirstName"];
+            ViewBag.Lastname = formCollection["LastName"];
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()  
+        {
+            ViewData["movie"] = "One Flew over";
+            ViewData["year"] = 1078;
+            ViewBag.Movie = "Forrest Gump";
+            ViewBag.Year = 1996;
+
+            return View( ); 
+        }
+    }
+}
